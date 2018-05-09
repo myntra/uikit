b5daf5a7b1be8977d7f2add3c2f8b29f8791d324
Rahul Kadyan <hi@znck.me>
2018-05-09 15:05:28 +0530

    chore: Remove Icon from .gitignore

---
+++ packages/@myntra/uikit-elements/src/Icon/Icon.jsx
@@ -0,0 +1,38 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { classnames } from '@myntra/uikit-utils'
+
+import styles from './Icon.css'
+
+import Promised from '../Promised/Promised'
+
+/**
+ General purpose SVG icon.
+
+ @since 0.0.0
+ @status EXPERIMENTAL
+ @example <Icon name="alert" />
+ */
+export default class Icon extends PureComponent {
+  static propTypes = {
+    /** Icon name. */
+    name: PropTypes.string
+  }
+
+  render() {
+    const { name } = this.props
+
+    return (
+      <Promised
+        fn={() => import(`./icons/${name}.svg`).then(m => m.default)}
+        renderLoading={() => <span className={classnames('icon', 'loading').use(styles)} />}
+        renderError={() => <span className={classnames('icon', 'unknown').use(styles)} />}
+        render={SvgIcon => (
+          <span className={classnames('icon').use(styles)} alt={name}>
+            <SvgIcon className={classnames('svg').use(styles)} />
+          </span>
+        )}
+      />
+    )
+  }
+}
