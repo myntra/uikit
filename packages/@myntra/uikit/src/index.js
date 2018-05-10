90d7864616df918acf8d8a47659e4d3064126a8e
Rahul Kadyan <rahul.kadyan@myntra.com>
2018-05-10 08:17:12 +0000

    chore: Use standard-version and lerna to publish releases
    * chore: Move markdown-to-jsx dependency to internals
    
    * chore: Use standard-version and lerna to publish releases
    
    * chore: Make packages publishable
    
    * chore: Add cross package dependencies
    
    * chore: Add package to combine all components

---
+++ packages/@myntra/uikit/src/index.js
@@ -0,0 +1,8 @@
+import * as tokens from '@myntra/tokens'
+import * as u from '@myntra/uikit-utils'
+
+export * from '@myntra/uikit-elements'
+export * from '@myntra/uikit-compounds'
+export * from '@myntra/uikit-patterns'
+
+export { tokens, u }
