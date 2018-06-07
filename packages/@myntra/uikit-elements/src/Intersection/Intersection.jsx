ffa8f97cae4e9e4e796806b930bdd1309c145be0
Rahul Kadyan <rahul.kadyan@myntra.com>
2018-06-07 11:09:56 +0000

    feat(elements|Intersection): Component to use IntersectionObserver API in a declarative way
    Merged in feat/Intersection (pull request #23)
    
    * feat(elements|Intersection): Component to use IntersectionObserver API in a declarative way
    
    * feat: `once` prop to prevent duplicate enter/leave events

---
+++ packages/@myntra/uikit-elements/src/Intersection/Intersection.jsx
@@ -0,0 +1,161 @@
+import React, { Component, Children } from 'react'
+import PropTypes from 'prop-types'
+import IntersectionObserver from 'intersection-observer-polyfill'
+import { isEqualShallow } from '@myntra/uikit-utils'
+
+/**
+ It is declarative-way of using [IntersectionObserver API](https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API). It can be used for cases like, virtualized-list, infinite-scroll, lazy-loading or animations when an element enters the viewport.
+
+ @since 0.0.0
+ @status EXPERIMENTAL
+ @example
+if (!this.ref) {
+  this.setState({ intersectionRatio: 0, isIntersecting: false, status: 'unknown' })
+  this.ref = React.createRef()
+}
+
+<div>
+  <p>Scroll to bottom right corner!</p>
+  <div style={{ height: '200px', width: '200px', overflow: 'auto' }} ref={this.ref}>
+    <div style={{ height: '400px', width: '400px', background: '#efefef' }} />
+    <Intersection
+      root={this.ref}
+      threshold={[0, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1]}
+      onEnter={() => this.setState({ status: 'entered' })}
+      onLeave={() => this.setState({ status: 'left' })}
+      onIntersection={({ isIntersecting, intersectionRatio }) => this.setState({ intersectionRatio, isIntersecting })}
+    >
+      <div ref={this.ref} style={{ padding: '20px', margin: '100px', width: '200px', background: 'red' }} />
+    </Intersection>
+    <div style={{ height: '400px', width: '400px', background: '#efefef' }} />
+  </div>
+  <p>
+    Visibility: {this.state.intersectionRatio * 100}% <br />
+    Intersecting: {''+this.state.isIntersecting} <br />
+    Status: {this.state.status}
+  </p>
+</div>
+ */
+class Intersection extends Component {
+  static propTypes = {
+    children: PropTypes.element.isRequired,
+    /**
+     * The element that is used as the viewport for checking visibility of the target. Must be a React reference to an ancestor element of the target.
+     * Defaults to the browser viewport if not specified or if `null`.
+     */
+    root: PropTypes.shape({
+      current: PropTypes.instanceOf(HTMLElement) // eslint-disable-line no-undef
+    }),
+    /**
+     * Margin around the root. Can have values similar to the CSS margin property, e.g. `"10px 20px 30px 40px"` (top, right, bottom, left).
+     * The values can be percentages. This set of values serves to grow or shrink each side of the root element's bounding box before
+     * computing intersections. Defaults to all zeros.
+     */
+    rootMargin: PropTypes.string,
+    /**
+     * Either a single number or an array of numbers which indicate at what percentage of the target's visibility
+     * the observer's callback should be executed. If you only want to detect when visibility passes the 50% mark,
+     * you can use a value of `0.5`. If you want the callback run every time visibility passes another 25%, you would
+     * specify the array `[0, 0.25, 0.5, 0.75, 1]`. The default is 0 (meaning as soon as even one pixel is visible,
+     * the callback will be run). A value of `1.0` means that the threshold isn't considered passed until every pixel is visible.
+     */
+    threshold: PropTypes.oneOfType([PropTypes.number, PropTypes.arrayOf(PropTypes.number)]),
+    /**
+     * Event fired when observed element enters the root element.
+     * [IntersectionObserverEntry](https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserverEntry)
+     * @type {function(IntersectionObserverEntry):void}
+     */
+    onEnter: PropTypes.func,
+    /**
+     * Event fired when observed element leaves the root element.
+     * @type {function(IntersectionObserverEntry):void}
+     */
+    onLeave: PropTypes.func,
+    /**
+     * Event fired when observed element intersection value changes.
+     * @type {function(IntersectionObserverEntry):void}
+     */
+    onIntersection: PropTypes.func,
+    /** Enter and Leave events should be fired only once. */
+    once: PropTypes.bool
+  }
+
+  static defaultProps = {
+    rootMargin: '0px 0px 0px 0px',
+    threshold: [0, 0.2]
+  }
+
+  constructor(props) {
+    super(props)
+    this.ref = current => {
+      this.ref.current = current
+    }
+  }
+
+  componentDidMount() {
+    this.startObserver()
+    this.observer.observe(this.ref.current)
+  }
+
+  componentWillUnmount() {
+    this.stopObserver()
+  }
+
+  componentWillUpdate() {
+    this.observer.unobserve(this.ref.current)
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.hasOptionsChanged(this.props, prevProps)) {
+      this.stopObserver()
+      this.startObserver()
+    }
+    this.observer.observe(this.ref.current)
+    this.lastEntry = null
+  }
+
+  hasOptionsChanged(newProps, oldProps) {
+    return (
+      newProps.rootMargin !== oldProps.rootMargin ||
+      !isEqualShallow(newProps.threshold, oldProps.threshold) ||
+      (this.observer && newProps.root ? this.observer.root !== newProps.root.current : false)
+    )
+  }
+
+  handleIntersection = entries => {
+    const target = entries[entries.length - 1]
+
+    if (this.props.once && this.lastEntry) {
+      if (this.lastEntry.isIntersecting && !target.isIntersecting) {
+        this.props.onLeave && this.props.onLeave(target)
+      } else if (!this.lastEntry.isIntersecting && target.isIntersecting) {
+        this.props.onEnter && this.props.onEnter(target)
+      }
+    } else {
+      if (target.isIntersecting) this.props.onEnter && this.props.onEnter(target)
+      else this.props.onLeave && this.props.onLeave(target)
+    }
+
+    this.lastEntry = target // remember entry to check enter/leave on next event.
+    this.props.onIntersection && this.props.onIntersection(target)
+  }
+
+  startObserver() {
+    this.observer = new IntersectionObserver(this.handleIntersection, {
+      threshold: this.props.threshold,
+      root: this.props.root ? this.props.root.current : null,
+      rootMargin: this.props.rootMargin
+    })
+  }
+
+  stopObserver() {
+    this.observer.disconnect()
+    this.observer = null
+  }
+
+  render() {
+    return React.cloneElement(Children.only(this.props.children), { ref: this.ref })
+  }
+}
+
+export default Intersection
---
+++ packages/@myntra/uikit-elements/src/Intersection/Intersection.spec.js
@@ -0,0 +1,227 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+
+jest.mock('intersection-observer-polyfill')
+import Intersection from './Intersection' // eslint-disable-line import/first
+import Mock from 'intersection-observer-polyfill' // eslint-disable-line import/first
+
+describe('Intersection', () => {
+  const onIntersection = jest.fn()
+  const onEnter = jest.fn()
+  const onLeave = jest.fn()
+
+  afterEach(() => {
+    onIntersection.mockClear()
+    onEnter.mockClear()
+    onLeave.mockClear()
+    Mock.clear()
+  })
+
+  it('should render first child', () => {
+    const wrapper = shallow(
+      <Intersection>
+        <div id="child">foo</div>
+      </Intersection>
+    )
+
+    expect(wrapper.find('#child')).toBeTruthy()
+    expect(wrapper.text()).toBe('foo')
+  })
+
+  it('should create IntersectionObserver instance', () => {
+    shallow(
+      <Intersection rootMargin="0px" threshold={[0]}>
+        <div id="child">foo</div>
+      </Intersection>
+    )
+
+    const observer = Mock.instances[0]
+
+    expect(observer.options.root).toBe(null)
+    expect(observer.options.rootMargin).toBe('0px')
+    expect(observer.options.threshold).toEqual([0])
+  })
+
+  it('should create IntersectionObserver instance with custom root', () => {
+    const ref = React.createRef()
+
+    const wrapper = shallow(
+      <Intersection root={ref} rootMargin="0px" threshold={[0]}>
+        <div id="child">foo</div>
+      </Intersection>
+    )
+
+    const observer = Mock.instances[0]
+
+    expect(observer.options.root).toBe(ref.current)
+    expect(observer.options.rootMargin).toBe('0px')
+    expect(observer.options.threshold).toEqual([0])
+
+    wrapper.update()
+
+    expect(Mock.instances).toHaveLength(1)
+  })
+
+  it('should create new IntersectionObserver instance if root changes', () => {
+    const ref = React.createRef()
+
+    const wrapper = mount(
+      <Intersection root={ref}>
+        <div id="child">foo</div>
+      </Intersection>
+    )
+
+    const observer = Mock.instances[0]
+
+    expect(observer.options.root).toBe(ref.current)
+
+    const root = React.createRef()
+    root.current = wrapper.find('#child').getDOMNode()
+    wrapper.setProps({ root })
+
+    expect(Mock.instances).toHaveLength(2)
+    expect(observer.options.root).toBe(ref.current)
+  })
+
+  it('should destroy when unmounted', () => {
+    const wrapper = shallow(
+      <Intersection>
+        <div id="child">foo</div>
+      </Intersection>
+    )
+
+    const observer = Mock.instances[0]
+    observer.disconnect = jest.fn()
+
+    wrapper.unmount()
+
+    expect(observer.disconnect).toBeCalled()
+  })
+
+  it('should change options when props change', () => {
+    const wrapper = shallow(
+      <Intersection>
+        <div id="child">foo</div>
+      </Intersection>
+    )
+
+    wrapper.setProps({ threshold: 0 })
+
+    expect(Mock.instances).toHaveLength(2)
+  })
+
+  it('should observe new children', () => {
+    const wrapper = shallow(
+      <Intersection>
+        <div key="foo">foo</div>
+      </Intersection>
+    )
+
+    expect(wrapper.text()).toBe('foo')
+
+    const observer = Mock.instances[0]
+    observer.observe = jest.fn()
+    observer.unobserve = jest.fn()
+
+    wrapper.setProps({ children: <div key="bar">bar</div> })
+
+    expect(wrapper.text()).toBe('bar')
+    expect(observer.observe).toBeCalled()
+    expect(observer.unobserve).toBeCalled()
+
+    wrapper.setProps({})
+
+    expect(Mock.instances).toHaveLength(1)
+  })
+
+  it('should fire onIntersection event', () => {
+    shallow(
+      <Intersection onIntersection={onIntersection}>
+        <div id="child">foo</div>
+      </Intersection>
+    )
+
+    const observer = Mock.instances[0]
+    const value = { isIntersecting: false }
+    observer.handler([value])
+
+    expect(onIntersection).toBeCalledWith(value)
+  })
+
+  it('should fire onEnter & onLeave events', () => {
+    shallow(
+      <Intersection onEnter={onEnter} onLeave={onLeave}>
+        <div id="child">foo</div>
+      </Intersection>
+    )
+
+    const observer = Mock.instances[0]
+    const value = { isIntersecting: true }
+
+    observer.handler([value])
+    expect(onEnter).toBeCalledWith(value)
+    expect(onLeave).not.toBeCalled()
+
+    onEnter.mockClear()
+    onLeave.mockClear()
+
+    value.isIntersecting = false
+
+    observer.handler([value])
+    expect(onEnter).not.toBeCalled()
+    expect(onLeave).toBeCalledWith(value)
+  })
+
+  it('should fire onEnter & onLeave events once', () => {
+    shallow(
+      <Intersection onEnter={onEnter} onLeave={onLeave} once>
+        <div id="child">foo</div>
+      </Intersection>
+    )
+
+    const observer = Mock.instances[0]
+    let value = { isIntersecting: true }
+
+    // -- intersecting
+    observer.handler([value])
+    expect(onEnter).toBeCalledWith(value)
+    expect(onLeave).not.toBeCalled()
+
+    onEnter.mockClear()
+    onLeave.mockClear()
+    // -- non-intersecting
+    value = { isIntersecting: false }
+    observer.handler([value])
+    expect(onEnter).not.toBeCalled()
+    expect(onLeave).toBeCalledWith(value)
+
+    onEnter.mockClear()
+    onLeave.mockClear()
+    // -- non-intersecting
+    // -- should not trigger if same event is trigger again.
+    value = { isIntersecting: false }
+
+    observer.handler([value])
+    expect(onEnter).not.toBeCalled()
+    expect(onLeave).not.toBeCalled()
+
+    onEnter.mockClear()
+    onLeave.mockClear()
+    // -- intersecting
+    value = { isIntersecting: true }
+
+    observer.handler([value])
+    expect(onEnter).toBeCalledWith(value)
+    expect(onLeave).not.toBeCalled()
+
+    onEnter.mockClear()
+    onLeave.mockClear()
+    // -- intersecting
+    // -- should not trigger if same event is trigger again.
+    value = { isIntersecting: true }
+
+    observer.handler([value])
+    expect(onEnter).not.toBeCalled()
+    expect(onLeave).not.toBeCalled()
+  })
+})
