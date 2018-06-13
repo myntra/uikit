975ee059df09ac9814ea36faded72f7d818c8add
srikanthmangipudi <srikanth.mangipudi@myntra.com>
2018-06-13 06:26:47 +0000

    feat(elements | InputNumber): Add InputNumber component
    Merged in feat/InputNumber (pull request #40)
    
    * feat(InputNumber): Add InputNumber component
    
    * refact: Convert onChange value to number
    
    * fix: Hide number spinner for un-focused input
    
    * chore: Sort exports in elements package

---
+++ packages/@myntra/uikit-elements/src/InputNumber/InputNumber.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { classnames } from '@myntra/uikit-utils'
+import PropTypes from 'prop-types'
+import styles from './InputNumber.css'
+
+/**
+ * The InputNumber component.
+ * @since 0.0.0
+ * @status EXPERIMENTAL
+ * @example
+ * <InputNumber value={this.state.value} onChange={value => { this.setState({ value }) }} />
+ */
+const InputNumber = ({ className, onChange, ...props }) => {
+  return (
+    <div className={className}>
+      <input
+        {...props}
+        type="number"
+        className={classnames('input').use(styles)}
+        onChange={event => onChange && onChange(parseFloat(event.target.value))}
+      />
+    </div>
+  )
+}
+
+InputNumber.propTypes = {
+  /** @private */
+  className: PropTypes.string,
+  /** Disable Input */
+  disabled: PropTypes.bool,
+  /** Value */
+  value: PropTypes.number,
+  /**
+   * Change event handler
+   * @function
+   * @param {number} value
+   */
+  onChange: PropTypes.func
+}
+
+InputNumber.defaultProps = {
+  disabled: false
+}
+
+export default InputNumber
