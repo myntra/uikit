c8d8f6c562d778e6c133e49f75928d1b2a8a2daa
Rahul Tiwari <rahul.tiwari@myntra.com>
2018-06-13 09:59:59 +0000

    feat(elements | InputTextarea): Add InputTextarea component
    Merged in feat/textarea (pull request #33)
    
    * Feat: Text area input (WIP)
    
    * Fix: Review comments incorporated.
    
    * Fix: Will have full width if 'cols' prop is falsy.
    
    * refactor: Rename to InputTextArea and reuse CSS
    
    Approved-by: Easwar Sivaramakrishnan <easwar.sivaramakrishnan@myntra.com>

---
+++ packages/@myntra/uikit-elements/src/InputTextArea/InputTextArea.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { classnames } from '@myntra/uikit-utils'
+import PropTypes from 'prop-types'
+import styles from './InputTextArea.css'
+
+/**
+ The textarea component.
+ @since 0.0.0
+ @status EXPERIMENTAL
+ @example
+ <InputTextArea value={this.state.value} onChange={value=>this.setState({value})} />
+ */
+function InputTextArea({ className, noResize, value, onChange, ...props }) {
+  return (
+    <div className={classnames(className, 'textarea').use(styles)}>
+      <textarea
+        {...props}
+        value={value || ''}
+        className={classnames('input', { 'no-resize': noResize }).use(styles)}
+        onChange={event => onChange && onChange(event.target.value)}
+      />
+    </div>
+  )
+}
+
+InputTextArea.propTypes = {
+  /** @private */
+  className: PropTypes.string,
+  /** Value */
+  value: PropTypes.string,
+  /**
+   * Handler for change event
+   * @function
+   * @param {string} value
+   */
+  onChange: PropTypes.func,
+  /** Disable Input */
+  disabled: PropTypes.bool,
+  /** Rows */
+  rows: PropTypes.number,
+  /** Disable resize and hide resize handle */
+  noResize: PropTypes.bool,
+  /** Placeholder */
+  placeholder: PropTypes.string
+}
+
+InputTextArea.defaultProps = {
+  transparent: false,
+  disabled: false,
+  noResize: false,
+  rows: 1
+}
+
+export default InputTextArea
