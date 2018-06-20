f380eb3be068c049b88de14aa74f54d8d69d67bd
Rahul Kadyan <hi@znck.me>
2018-06-20 08:48:55 +0530

    chore: Generate static routes for components

--- scripts/create-static-routes.js
+++ scripts/create-static-routes.js
@@ -1,16 +1,39 @@
 const nav = require('../docs/_app/nav')
 const { execSync } = require('child_process')
 const { resolve, join } = require('path')
+const { readFileSync } = require('fs')
+
+function getExports(filename) {
+  const content = readFileSync(resolve(__dirname, '../packages/@myntra', filename)).toString()
+  const names = {}
+
+  content.replace(/export \{ default as ([^\s]+) \} from '[^']+'/g, (_, name) => {
+    names[name] = true
+  })
+
+  return names
+}
 
 const dist = resolve(__dirname, '../dist')
+const components = {
+  compounds: getExports('uikit-compounds/src/index.js'),
+  elements: getExports('uikit-elements/src/index.js'),
+  internals: getExports('uikit-internals/src/index.js'),
+  patterns: getExports('uikit-patterns/src/index.js')
+}
 
-nav.forEach(({ to }) => {
-  if (!to || to === '/') return
+function createSymlinks(items) {
+  items.forEach(({ to, children }) => {
+    if (!to || to === '/') return
 
-  to = to.substr(1)
+    to = to.substr(1)
 
-  const target = join(dist, to)
-  execSync(`mkdir -p ${target}`)
-  execSync(`cp ${dist}/index.html ${target}/index.html`)
-  execSync(`cp ${dist}/index.html ${target}/200.html`)
-})
+    const target = join(dist, to)
+    execSync(`mkdir -p ${target}`)
+    execSync(`cp ${dist}/index.html ${target}/index.html`)
+
+    children && createSymlinks(children(components))
+  })
+}
+
+createSymlinks(nav)
