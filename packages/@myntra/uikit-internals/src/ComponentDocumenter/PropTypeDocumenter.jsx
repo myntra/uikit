830902e8edbc325939a2273539ee669f7a0dca1c
Rahul Kadyan <hi@znck.me>
2018-06-20 15:06:50 +0530

    feat: Improve JSDoc support in component documenter

--- packages/@myntra/uikit-internals/src/ComponentDocumenter/PropTypeDocumenter.jsx
+++ packages/@myntra/uikit-internals/src/ComponentDocumenter/PropTypeDocumenter.jsx
@@ -3,7 +3,100 @@ import PropTypes from 'prop-types'
 import { Dropdown } from '@myntra/uikit-elements'
 import styles from './PropTypeDocumenter.css'
 
-export default function PropTypeDocumenter({ name, value, raw, meta = [] } = {}) {
+function JSDocTypeDefDocumenter({ reference, rootReference, type, name }) {
+  console.log(name, type)
+  switch (type.name) {
+    case 'function': {
+      const params = reference.filter(it => it.title === 'param')
+      const returns = reference.find(it => it.title === 'returns') || { type: null }
+      return (
+        <span>
+          function({params.map((param, index) => (
+            <span key={param.name}>
+              {param.name}: <JSDocTypeDocumenter {...param.type} rootReference={rootReference} />
+              {index + 1 < params.length && <span>, </span>}
+            </span>
+          ))}):{' '}
+          <JSDocTypeDocumenter type="NameExpression" name="void" {...returns.type} rootReference={rootReference} />
+        </span>
+      )
+    }
+    default:
+      return <JSDocTypeDocumenter {...type} rootReference={rootReference} />
+  }
+}
+
+function JSDocTypeDocumenter({ reference = [], rootReference = [], ...param }) {
+  const references = [].concat(reference, rootReference)
+  switch (param.type) {
+    case 'NameExpression': {
+      for (const item of references) {
+        const typedef = item.find(item => item.title === 'typedef' && item.name === param.name)
+
+        if (typedef) return <JSDocTypeDefDocumenter {...typedef} reference={item} rootReference={references} />
+      }
+
+      return <span>{param.name}</span>
+    }
+    case 'NullableType': {
+      return (
+        <span>
+          {param.prefix && '?'}
+          <JSDocTypeDocumenter {...param.expression} rootReference={references} />
+          {!param.prefix && '?'}
+        </span>
+      )
+    }
+    case 'UnionType': {
+      return param.elements.map((element, index) => (
+        <span key={index}>
+          <JSDocTypeDocumenter {...element} rootReference={references} />
+          {index + 1 < param.elements.length && <span> | </span>}
+        </span>
+      ))
+    }
+    case 'RecordType': {
+      return (
+        <span>
+          {'{ '}
+          {param.fields.map((field, index) => (
+            <span key={index}>
+              <JSDocTypeDocumenter {...field} keyName={field.key} rootReference={references} />
+              {index + 1 < param.fields.length && <span>, </span>}
+            </span>
+          ))}
+          {' }'}
+        </span>
+      )
+    }
+    case 'FieldType': {
+      return (
+        <span>
+          {param.keyName}: <JSDocTypeDocumenter {...param.value} rootReference={references} />
+        </span>
+      )
+    }
+    case 'TypeApplication': {
+      return (
+        <span>
+          <JSDocTypeDocumenter {...param.expression} />
+          {'<'}
+          {param.applications.map((application, index) => (
+            <span key={index}>
+              <JSDocTypeDocumenter reference={references} {...application} />
+              {index + 1 < param.applications.length && <span>, </span>}
+            </span>
+          ))}
+          {'>'}
+        </span>
+      )
+    }
+    default:
+      return JSON.stringify(param)
+  }
+}
+
+export default function PropTypeDocumenter({ name, value, raw, meta = [], reference = [] } = {}) {
   switch (name) {
     case 'enum':
       return (
@@ -17,15 +110,73 @@ export default function PropTypeDocumenter({ name, value, raw, meta = [] } = {})
           </div>
         </Dropdown>
       )
-    case 'func':
+    case 'union':
       return (
-        <span className={styles.func}>
-          function({meta
-            .filter(it => it.title === 'param')
-            .map(param => param.name + ': ' + param.type.name)
-            .join(', ')}): {(meta.find(it => it.title === 'returns') || { type: { name: 'void' } }).type.name}
+        <span>
+          {value.map((item, index) => (
+            <span key={index}>
+              <PropTypeDocumenter {...item} reference={reference} />
+              {index + 1 < value.length && <span> | </span>}
+            </span>
+          ))}
         </span>
       )
+    case 'custom':
+      console.log('CUSTOM', raw, reference)
+      return (
+        <span>
+          <JSDocTypeDocumenter name={raw} type="NameExpression" rootReference={reference} />
+        </span>
+      )
+    case 'arrayOf':
+      return (
+        <span>
+          Array{'<'}
+          <PropTypeDocumenter {...value} reference={reference} />
+          {'>'}
+        </span>
+      )
+    case 'shape':
+      const items = Object.entries(value)
+      return (
+        <span>
+          {'{ '}
+          {items.map(([key, item], index) => (
+            <span key={index}>
+              {key}: <PropTypeDocumenter {...item} reference={reference} />
+              {index + 1 < items.length && <span>, </span>}
+            </span>
+          ))}
+          {' }'}
+        </span>
+      )
+    case 'object': {
+      const type = meta.find(it => it.title === 'type') || { type: null }
+      return (
+        <span>
+          <JSDocTypeDocumenter type="NameExpression" name="object" {...type.type} rootReference={reference} />
+        </span>
+      )
+    }
+    case 'func': {
+      const type = meta.find(it => it.title === 'type')
+      const params = meta.filter(it => it.title === 'param')
+      const returns = meta.find(it => it.title === 'returns') || { type: null }
+      return type ? (
+        <span>
+          <JSDocTypeDocumenter {...type} rootReference={reference} />
+        </span>
+      ) : (
+        <span>
+          function({params.map((param, index) => (
+            <span key={param.name}>
+              {param.name}: <JSDocTypeDocumenter {...param.type} rootReference={reference} />
+              {index + 1 < params.length && <span>, </span>}
+            </span>
+          ))}): <JSDocTypeDocumenter type="NameExpression" name="void" {...returns.type} rootReference={reference} />
+        </span>
+      )
+    }
     default:
       return <span>{name}</span>
   }
@@ -35,6 +186,7 @@ PropTypeDocumenter.propTypes = {
   name: PropTypes.string,
   value: PropTypes.any,
   meta: PropTypes.array,
+  reference: PropTypes.array,
   raw: PropTypes.string
 }
 
