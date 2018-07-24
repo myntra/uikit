373174166921262412823e5af032580599d9720c
Rahul Kadyan <hi@znck.me>
2018-07-24 16:25:35 +0530

    chore(scripts): Add publish script

---
+++ scripts/publish.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Get current directory.
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR="$( dirname "$DIR" )"
+
+PACKAGES=$(ls "${PROJECT_DIR}/packages/@myntra/")
+
+for PACKAGE in ${PACKAGES}; do
+  cd "${PROJECT_DIR}/packages/@myntra/${PACKAGE}"
+  echo "=> Publishing @myntra/${PACKAGE}"
+  echo "   Version: $(node -e 'const { version } = require("./package.json"); console.log(version)')"
+  PRIVATE="$(node -e 'const { private } = require("./package.json"); console.log(private ? "true" : "false")')"
+
+  if [ "${PRIVATE}" = "false" ]; then
+    npm publish &> /dev/null && echo "   Done" || echo "   Already published."
+  else
+    echo "   Private package."
+  fi
+
+  echo ""
+  cd - &> /dev/null
+done
