82449403a3de37ccb61436e1502611e16866458f
Rahul Kadyan <hi@znck.me>
2018-10-03 19:54:13 +0530

    chore: Use tag from version

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,7 +1,12 @@
+const { resolve } = require('path')
 const { execSync } = require('child_process')
+const { version } = require(resolve(process.cwd(), 'package.json'))
 
 try {
-  execSync('npm publish', { stdio: 'pipe' }, true)
+  const isAlpha = /alpha/.test(version)
+  const isBeta = /beta/.test(version)
+  const tag = isAlpha ? 'alpha' : isBeta ? 'beta' : 'latest'
+  execSync(`npm publish --tag ${tag}`, { stdio: 'pipe' }, true)
   console.log('Done.')
 } catch (ex) {
   const output = ex.stderr.toString()
