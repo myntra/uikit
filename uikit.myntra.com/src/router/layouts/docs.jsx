da125972aa655afcbf9c4ffff7734240c9973843
Rahul Kadyan <hi@znck.me>
2019-09-11 11:07:10 +0530

    chore: Remove v0 banner

--- uikit.myntra.com/src/router/layouts/docs.jsx
+++ uikit.myntra.com/src/router/layouts/docs.jsx
@@ -1,23 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import DefaultLayout from './default'
-import { Alert } from '@myntra/uikit'
 
 import './docs.css'
 
 export default function DocsPageLayout({ children }) {
   return (
     <DefaultLayout>
-      <div className="docs-page-layout">
-        <Alert type="warning" title="NOTICE">
-          This is documentation corresponds to UIKit v1.0. Go to{' '}
-          <a href="//v0-uikit.myntra.com" target="_blank" rel="noopener noreferrer">
-            v0-uikit.myntra.com
-          </a>{' '}
-          for previous versions.
-        </Alert>
-        {children}
-      </div>
+      <div className="docs-page-layout">{children}</div>
     </DefaultLayout>
   )
 }
